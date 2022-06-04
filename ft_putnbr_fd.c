a6e5431aea3d49ae09ea315c12c6844bf92df3ef
Caesar Augustus <wiccaesar@hotmail.com>
2022-06-04 03:05:01 -0300

    Incluído putnbr_fd.

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,12 +6,35 @@
 /*   By: cnascime <cnascime@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 18:45:22 by cnascime          #+#    #+#             */
-/*   Updated: 2022/05/25 16:11:41 by cnascime         ###   ########.fr       */
+/*   Updated: 2022/06/04 01:56:36 by cnascime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-n: The integer to output.
-fd: The file descriptor on which to write.
-Outputs the integer ’n’ to the given file descriptor.
-*/
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned char	c;
+
+	if (n < 0)
+	{
+		if (n == -2147483648)
+		{
+			write(fd, "-2147483648", 11);
+			return (0);
+		}
+		else
+		{
+			write(fd, "-", 1);
+			n *= -1;
+		}
+	}
+	if (n < 10)
+	{
+		c = n + '0';
+		write(fd, &c, sizeof(c));
+	}
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+}
