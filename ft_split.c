28964af82a9deb953778183d954f98c4714458c7
Caesar Augustus <wiccaesar@hotmail.com>
2022-08-04 02:36:47 -0300

    Added  gmasid's split as libft's default.
    Out of convenience just because it frees.

--- ft_split.c
+++ ft_split.c
@@ -5,109 +5,102 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cnascime <cnascime@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/13 18:07:44 by cnascime          #+#    #+#             */
-/*   Updated: 2022/06/09 19:15:51 by cnascime         ###   ########.fr       */
+/*   Created: 2022/05/10 14:28:20 by gmasid            #+#    #+#             */
+/*   Updated: 2022/08/04 02:31:57 by cnascime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+// This version of libft frees.
+// It was made by gmasid, and I just copied it out of convenience.
 
-static int		ft_shardcounter(char *altogether, char forbidden);
-static int		ft_isforbidden(char character, char forbidden);
-static char		*ft_superbonder(char *altogether, char forbidden);
-static int		ft_shardsize(char *altogether, char forbidden);
+#include "libft.h"
 
-// Glues ~shards~ in a single ~array~ again, filtering out forbidden characters.
+static int	count_words(const char *str, char c);
+static char	*dup_word(const char *str, int start, int finish);
+static void	free_result(char **result, size_t j);
+static char	**fill_result(char **result, const char *s, char c);
+
 char	**ft_split(char const *s, char c)
 {
-	int		shard;
-	int		count;
-	char	**kintsugi;
+	char	**result;
 
 	if (!s)
-		return (NULL);
-	shard = 0;
-	count = ft_shardcounter((char *)s, c);
-	kintsugi = malloc(sizeof(char *) * (count + 1));
-	if (!kintsugi)
-	{
-		free (kintsugi);
-		return (NULL);
-	}
-	while (shard < count)
-	{
-		while (ft_isforbidden(*s, c))
-			s++;
-		kintsugi[shard] = ft_superbonder((char *)s, c);
-		while (!ft_isforbidden(*s, c))
-			s++;
-		shard++;
-	}
-	kintsugi[shard] = 0;
-	return (kintsugi);
+		return (0);
+	result = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!result)
+		return (0);
+	result = fill_result(result, s, c);
+	return (result);
 }
 
-// Counts the amount of shards to reveal the array size.
-static int	ft_shardcounter(char *altogether, char forbidden)
+static int	count_words(const char *str, char c)
 {
 	int	i;
-	int	shardcount;
+	int	trigger;
 
 	i = 0;
-	shardcount = 0;
-	while (altogether[i] != '\0')
+	trigger = 0;
+	while (*str)
 	{
-		while (*altogether && ft_isforbidden(*altogether, forbidden))
-			altogether++;
-		while (*altogether && !ft_isforbidden(*altogether, forbidden))
-			altogether++;
-		if (*altogether || !ft_isforbidden(*(altogether - 1), forbidden))
-			shardcount++;
+		if (*str != c && trigger == 0)
+		{
+			trigger = 1;
+			i++;
+		}
+		else if (*str == c)
+			trigger = 0;
+		str++;
 	}
-	return (shardcount);
+	return (i);
 }
 
-// Checks whether the character in question is forbidden.
-// 1-true if it's forbidden, 2-false if it's allowed.
-static int	ft_isforbidden(char character, char forbidden)
+static char	*dup_word(const char *str, int start, int finish)
 {
-	if (character == forbidden)
-		return (1);
-	if (character == '\0')
-		return (1);
-	return (0);
-}
-
-// Glues valid ~characters~ in a row in their own ~shard~, one by one.
-static char	*ft_superbonder(char *altogether, char forbidden)
-{
+	char	*word;
 	int		i;
-	char	*shard;
 
-	shard = malloc(sizeof(char) * (ft_shardsize(altogether, forbidden) + 1));
-	if (!shard)
-		return (NULL);
 	i = 0;
-	while (!ft_isforbidden(*altogether, forbidden))
-	{
-		shard[i] = *altogether;
-		i++;
-		altogether++;
-	}
-	shard[i] = '\0';
-	return (shard);
+	word = malloc((finish - start + 1) * sizeof(char));
+	while (start < finish)
+		word[i++] = str[start++];
+	word[i] = 0;
+	return (word);
 }
 
-// Just measures the necessary amount of bytes for memory allocation.
-static int	ft_shardsize(char *altogether, char forbidden)
+static void	free_result(char **result, size_t j)
 {
-	int	size;
-
-	size = 0;
-	while (!ft_isforbidden(*altogether, forbidden))
+	while (j--)
 	{
-		size++;
-		altogether++;
+		free(result[j]);
 	}
-	return (size);
+	free(result);
+}
+
+static char	**fill_result(char **result, const char *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	int		dup_start_index;
+
+	i = 0;
+	j = 0;
+	dup_start_index = -1;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && dup_start_index < 0)
+			dup_start_index = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && dup_start_index >= 0)
+		{
+			result[j] = dup_word(s, dup_start_index, i);
+			if (!result[j++])
+			{
+				free_result(result, j);
+				return (NULL);
+			}
+			dup_start_index = -1;
+		}
+		i++;
+	}
+	result[j] = 0;
+	return (result);
 }
